ca3059ea2148c709e628930d62e028d5adf8ce8b
KiranMdhr <kiranmdhr9@gmail.com>
2024-04-03 22:27:11 +0545

    this keyword,use of paranthesis()

---
+++ arrow.js
@@ -0,0 +1,37 @@
+// const user = {
+//   username: "kiran",
+//   price: 999,
+
+//   welcomeMessage: function () {
+//     console.log(`${this.username} ,Welcome to website`);
+//     console.log(this);
+//   },
+// };
+// user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
+
+// function one() {
+//   let username = "kiran";
+//   console.log(this.username);
+// }
+// one();
+
+// const heelo = function () {
+//   let name = "kiran";
+
+//   console.log(this.name);
+// };
+
+// const one = () => {
+//   let username = "jhuv";
+//   console.log(this.username);
+// };
+
+// one();
+// curly braces use vayo vane return keyword lekhnu parxa ani () mah wrap garyo vane return lekhnu pardaina
+const addTw0 = (no1, no2) => no1 + no2;
+const addThree = (no1, no2, no3) => (result = no1 + no2 + no3);
+
+console.log(addTw0(1, 2));
+console.log(addThree(1, 2, 3));
