45971a42eb13c28ee48115a8511ca1eab4901adf
KiranMdhr <kiranmdhr9@gmail.com>
2024-04-03 11:46:24 +0545

    conversion of datatypes

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+console.log(3 + 3);
+
+let name = "kiran";
+let age = 14;
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //pbject
+
+let score = "false";
+let value = Number(score);
+console.log(value);
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 in boolean true  0 => false
+// " " => false
+// "kiran" => true
+
+let a = 234;
+let convert = String(a);
+console.log(typeof convert);
---
+++ variables.js
@@ -0,0 +1,18 @@
+const accountId = 1222345;
+let accountEmail = "kiranmdhr9@gmail.accountEmail";
+var accountPassword = "12345";
+accountCity = "Kathmandu";
+let accountState;
+// accountId = 23123;
+
+accountEmail = "demo@gmail.com";
+accountPassword = "44444";
+accountCity = "Pokhara";
+
+console.table([
+  accountEmail,
+  accountId,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
